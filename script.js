ec1bb2576b4c1d377d5eff795e0ca3cc3454afd6
BHARADWAJ/ <91771191+BHARADWAJCODE@users.noreply.github.com>
2024-04-12 09:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,59 +6,60 @@ const skipButton = document.getElementById('skip');
 let score = 0;
 let currentQuestionIndex = 0;
 
-// Define quiz questions and options
 const questions = [
-    {
-      image: 'question1.png',
-      options: ['35/27', '1', '27/28', '28/27'],
-      correctOption: 3
-    },
-    {
-      image: 'question2.png',
-      options: ['7', '6', '9', '8'],
+  {
+    image: 'question1.png',
+    options: ['35/27', '1', '27/28', '28/27'],
+    correctOption: 3
+  },
+  {
+    image: 'question2.png',
+    options: ['7', '6', '9', '8'],
+    correctOption: 2
+  },
+  {
+    image: 'question3.png',
+    options: ['10:6:4', '9:6:4', '4:6:9', '6:10:4'],
+    correctOption: 1
+  },
+  {
+    image: 'question4.png',
+    options: ['-110', '-115', '-120', '115'],
+    correctOption: 1
+  },
+  {
+    image: 'question5.png',
+    options: ['4/3', '2/3', '3/4', '3/2'],
+    correctOption: 0
+  },
+  {
+    image: 'question6.png',
+    options: ['800', '890', '790', '690'],
+    correctOption: 2
+  },
+  {
+    image: 'question7.png',
+    options: ['p=10, q=42', 'p=11, q=41', 'p=12, q=40', 'None of these'],
+    correctOption: 1
+  },
+  {
+    image: 'question8.png',
+    options: ['6', '7', '8', '9'],
+    correctOption: 2
+  },
+  {
+      image: 'question9.png',
+      options: ['-2', '-3', '4', '2'],
       correctOption: 2
     },
-    {
-      image: 'question3.png',
-      options: ['10:6:4', '9:6:4', '4:6:9', '6:10:4'],
-      correctOption: 1
-    },
-    {
-      image: 'question4.png',
-      options: ['-110', '-115', '-120', '115'],
-      correctOption: 1
-    },
-    {
-      image: 'question5.png',
-      options: ['4/3', '2/3', '3/4', '3/2'],
-      correctOption: 0
-    },
-    {
-      image: 'question6.png',
-      options: ['800', '890', '790', '690'],
-      correctOption: 2
-    },
-    {
-      image: 'question7.png',
-      options: ['p=10, q=42', 'p=11, q=41', 'p=12, q=40', 'None of these'],
-      correctOption: 1
-    },
-    {
-      image: 'question8.png',
-      options: ['6', '7', '8', '9'],
-      correctOption: 2
-    },
-    {
-        image: 'question9.png',
-        options: ['-2', '-3', '4', '2'],
-        correctOption: 2
-      },
-    {
-      image: 'question10.png',
-      options: ['-55/119', '-55/109', '-54/119', '-54/109'],
-      correctOption: 1
-    }
-  ];
+  {
+    image: 'question10.png',
+    options: ['-55/119', '-55/109', '-54/119', '-54/109'],
+    correctOption: 1
+  }
+];
+
+
 
 // Function to load the current question
 function loadQuestion() {
